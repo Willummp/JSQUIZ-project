d774e620bc3dcfbc5888b8f00dbac8283cb0e006
Lucas Ferreira <lsousaah@gmail.com>
2022-09-20 11:56:08 -0300

    Primeira edição

---
+++ src/script.ts
@@ -0,0 +1,33 @@
+
+
+
+//TO DO: MÉTODO PARA CONTAR A PONTUAÇÃO
+
+
+try{
+    class Jogador1 { // classe do jogador1
+        private nome: string;
+        private pontuacao: number;
+        
+        constructor({nome,pontuacao}){
+          this.nome=nome;
+          this.pontuacao=pontuacao;
+        }
+    }
+    
+    class Jogador2 {
+      private nome: string;
+      private pontuacao: number;
+
+      constructor({nome,pontuacao}){
+        this.nome=nome;
+        this.pontuacao=pontuacao;
+      }
+    }
+
+
+
+
+} catch(error){
+
+} 
