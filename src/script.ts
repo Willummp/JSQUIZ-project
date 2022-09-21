b361422bc3f877b90a31d4996638107e0bacea1a
IsmaelMF9469 <ismaelmelo9469@hotmail.com>
2022-09-21 14:20:34 -0300

    adicionado

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,29 @@
 import PromptSync = require('prompt-sync')
 import { Jogador } from "./jogadores";
- 
+
+
+let option = 0;
+        console.clear();
+do {
+    console.log(' -- Java Game -- \n')
+    console.log(' [1] Novo Jogo ')
+    console.log(' [2] Tocar / Desativar Música')
+    console.log(' [3] Sair ')
+
+    option = Number(prompt(' >> '));
+
+    switch (option){
+        case 1:
+            const nomeJogador = prompt(' Digite seu nome: ');
+            const jogador = new Jogador (nomeJogador)
+            console.log(
+            
+        break;
+        case 2:
+            // TODO Colocar a função da música
+        break;
+        case 3:
+            // TODO SAIR DO JOGO
+        break;
+    }
+}while (option != 3)
