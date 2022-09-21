54f2a1ce06e3c71365f70c8220f045b80d6bdc98
Lucas Ferreira <lsousaah@gmail.com>
2022-09-21 10:00:45 -0300

    Classe Jogador Adicionada

--- src/jogadores.ts
+++ src/jogadores.ts
@@ -1,23 +1,17 @@
-
-
-
 //TODO: MÉTODO PARA CONTAR A PONTUAÇÃO
 interface NewJogador{
   nome: string;
   pontuacao: number;
-
+  nExperiencia: string;
 }
-
 class Jogador { // classe jogador
-    private nome: string;
-    private pontuacao: number;
-      
-     constructor({nome,pontuacao}){
+    public nome: string;
+    public pontuacao: number;
+    public nivelExperiencia: string;
+
+      constructor({nome,pontuacao}){
         this.nome=nome;
         this.pontuacao=pontuacao;
+        this.nivelExperiencia=this.nivelExperiencia;
       }
-}
-
-
-
- export { Jogador }
+} export { Jogador }
