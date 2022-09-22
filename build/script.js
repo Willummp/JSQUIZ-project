7ec2914eb2bf9d62cbaa5f1f1603dd9347926206
Lucas Ferreira <lsousaah@gmail.com>
2022-09-22 15:17:22 -0300

    changes

--- build/script.js
+++ build/script.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var PromptSync = require("prompt-sync");
 var jogadores_1 = require("./jogadores");
 var showMenu_1 = require("./showMenu");
+var Music_1 = require("./Music");
 var PerguntasRespostas_1 = require("./PerguntasRespostas");
 try {
     var prompt_1 = PromptSync();
@@ -11,8 +12,38 @@ try {
     var perguntas = new PerguntasRespostas_1.PerguntasRespostas();
     var respostas = new PerguntasRespostas_1.PerguntasRespostas();
     //Banco de Dados das Perguntas e Respostas
-    perguntas.perguntas = []; //Armazenar As Perguntas
-    respostas.respostas = []; //Armazenar As Respostas
+    perguntas.perguntas = ['O que as variáveis fazem ?',
+        'O que os vetores armazenam ?',
+        'O que as matrizes armazenan ?',
+        'A metodologia Ágil tem quantos valores ?',
+        'Quantos princípios a metodologia Ágil tem ?',
+        'Quais são os 3 papéis da metodologia ágil ?',
+        'O que é HTML ?',
+        'O que é HARDWARE ?',
+        'O que é SOFTWARE ?',
+        'Os exemplos de HARDWARE são ?',
+        'Os exemplos de SOFTWARE são ?',
+        'Podemos definir informática,como ?',
+        'Qual a função da RAM?',
+        'Defina o sistema operacional:',
+        'O que é um banco de dados?'];
+    //Armazenar As Perguntas
+    respostas.respostas = ['Armazenam apenas uma informação.',
+        'Armazenam várias informações em uma dimensão.',
+        'Armazena várias informações em várias dimensões.',
+        'Tem 4 valores.',
+        'Tem 12 princípios',
+        'Product Owner(P.O), Srum Master, Dev Team',
+        'É uma linguagem escrita',
+        'A parte física do computador',
+        'A parte lógica do computador',
+        'Placa-mãe, Memória e Bateria',
+        'Windows, Linux e Calculadora',
+        'A ciência que trata logicamente os dados',
+        'Armazenar temporariamente os programas e arquivos aberto para que o processador possa acessa-los',
+        'Aplicativo que interage entre o usuário e o sistema',
+        'Bancos de dados são coleções de informações que se relacionam de forma a criar um sentido'
+    ]; //Armazenar As Respostas
     //music.filePath = './musicas/trilha-sonora.mp3'; //Caminho do Diretório da Trilha Sonora do Jogo
     var option = 0;
     do {
@@ -23,26 +54,20 @@ try {
         switch (option) {
             case 1:
                 var nomeJogador = prompt_1(' Digite seu nome: ');
-                if (nomeJogador == null) {
-                    console.log('Valor Inválido, tente novamente !');
-                }
-                else {
-                    //IMPLEMENTAR 
-                }
                 var jogador = new jogadores_1.Jogador(nomeJogador);
                 break;
             case 2:
                 //music.playSound(option);
+                new Music_1.Music();
                 break;
             case 3:
                 console.log('GAME OVER');
                 break;
-            default:
-                console.log('Opção Inválida.');
         }
     } while (option > 0 && option != 3);
 }
 catch (error) {
     //Caso Ocorra Algum Erro
     console.log("Opss! Algo de errado aconteceu, tente novamente mais tarde :(\n".concat(error));
+    (0, showMenu_1.showMenu)();
 }
